20113508b1ce004219246f3bdd44c3998814862c
Derek Marchese <derekmarchese1@gmail.com>
2023-11-20 20:55:35 -0500

    imported csv and initialized variables

--- crawler.py
+++ crawler.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+# initilize list or array to store the scraped data
+products = []
+
+# a list of discovered urls with the first one to start
+urls = ["https://scrapeme.live/shop/"]
 
