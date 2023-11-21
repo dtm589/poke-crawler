863e19749bb485a8e4c00cae50452aee08bd385b
Derek Marchese <derekmarchese1@gmail.com>
2023-11-20 21:00:04 -0500

    add crawling

--- crawler.py
+++ crawler.py
@@ -8,3 +8,17 @@ products = []
 # a list of discovered urls with the first one to start
 urls = ["https://scrapeme.live/shop/"]
 
+# continue until all urls have been visited
+while len(urls) != 0:
+    # get the page to visit from the list
+    current_url = urls.pop()
+    
+    responce = requests.get(current_url)
+    soup = BeautifulSoup(responce.content, 'html.parser')
+    
+    link_elements = soup.select("a[href]")
+    
+    for element in link_elements:
+        url = element["href"]
+        if "https://scrapeme.live/shop" in url:
+            urls.append(url)
