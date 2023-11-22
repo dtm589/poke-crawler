80344fb3942c2c302dcbdb7f47bc21e0f5f1b134
Derek Marchese <derekmarchese1@gmail.com>
2023-11-21 23:51:58 -0500

    added priority queue

--- crawler.py
+++ crawler.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import queue
+import re
 import csv
 
 # initilize list or array to store the scraped data
 products = []
 
-# a list of discovered urls with the first one to start
-urls = ["https://scrapeme.live/shop/"]
+# create queue
+urls = queue.PriorityQueue()
+urls.put((0.5, "https://scrapeme.live/shop/"))
+
 visited_urls = []
 
 # continue until all urls have been visited
-while len(urls) != 0:
-    # get the page to visit from the list
-    current_url = urls.pop()
+while not urls.empty() and len(visited_urls) < 50:
+    #ignore the priority value
+    _, current_url = urls.get()
     
     response = requests.get(current_url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -25,15 +29,20 @@ while len(urls) != 0:
     for element in link_elements:
         url = element["href"]
         if "https://scrapeme.live/shop" in url:
-            if url not in visited_urls and url not in urls:
-                urls.append(url)
+            if url not in visited_urls and url not in [item[1] for item in urls.queue]:
+                #default score
+                priority_score = 1
+                #if it is a pagination page
+                if re.match(r"^https://scrapeme\.live/shop//\d+/?$", url):
+                    priority_score = 0.5
+                urls.put((priority_score, url))
             
     # if the current url is a product page
     product = {}
     product["url"] = current_url
     product["image"] = soup.select_one(".wp-post-image")["src"]
     product["name"] = soup.select_one(".product_title")
-    product["price"] = soup.select_one(".amount")
+    product["price"] = soup.select_one(".price")
     
     products.append(product)
     
